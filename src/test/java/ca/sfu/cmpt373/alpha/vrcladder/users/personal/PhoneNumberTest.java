090f43b72dc30841ddad1953243aee842c938a62
dquong <dquong@sfu.ca>
2016-05-30 22:16:16 -0700

    Add phone number verification
    Add phone number format verification. A phone number is a 10 - 11 digit,
    ignoring spaces, dashes, and parenthesis. Also create PhoneNumberTest to
    validate such.

---
+++ src/test/java/ca/sfu/cmpt373/alpha/vrcladder/users/personal/PhoneNumberTest.java
@@ -0,0 +1,55 @@
+package ca.sfu.cmpt373.alpha.vrcladder.users.personal;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PhoneNumberTest {
+
+    private static final String VALID_PHONE_NUM_SIMPLE = "1234567890";
+    private static final String VALID_PHONE_NUM_DASHES = "1-800-123-4567";
+    private static final String VALID_PHONE_NUM_SPACES = "604 123 4567";
+    private static final String VALID_PHONE_NUM_MIXED = "(604) 123-4567";
+
+    private static final String VALID_PHONE_NUM_1_PROCESSED = VALID_PHONE_NUM_SIMPLE;
+    private static final String VALID_PHONE_NUM_2_PROCESSED = "18001234567";
+    private static final String VALID_PHONE_NUM_3_PROCESSED = "6041234567";
+    private static final String VALID_PHONE_NUM_4_PROCESSED = VALID_PHONE_NUM_3_PROCESSED;
+
+    private static final String INVALID_PHONE_NUM_LETTERS = "604-abc-1234";
+    private static final String INVALID_PHONE_NUM_TOO_LONG = "604-1234-5678-91011";
+
+    @Test
+    public void testValidPhoneNumber() {
+        PhoneNumber phoneNumber = new PhoneNumber(VALID_PHONE_NUM_SIMPLE);
+        Assert.assertEquals(VALID_PHONE_NUM_1_PROCESSED, phoneNumber.toString());
+    }
+
+    @Test
+    public void testValidPhoneNumberWithDashes() {
+        PhoneNumber phoneNumber = new PhoneNumber(VALID_PHONE_NUM_DASHES);
+        Assert.assertEquals(VALID_PHONE_NUM_2_PROCESSED, phoneNumber.toString());
+    }
+
+    @Test
+    public void testValidPhoneNumberWithSpaces() {
+        PhoneNumber phoneNumber = new PhoneNumber(VALID_PHONE_NUM_SPACES);
+        Assert.assertEquals(VALID_PHONE_NUM_3_PROCESSED, phoneNumber.toString());
+    }
+
+    @Test
+    public void testValidPhoneNumberWithMixedCharcaters() {
+        PhoneNumber phoneNumber = new PhoneNumber(VALID_PHONE_NUM_MIXED);
+        Assert.assertEquals(VALID_PHONE_NUM_4_PROCESSED, phoneNumber.toString());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidPhoneNumberWithLetters() {
+        PhoneNumber phoneNumber = new PhoneNumber(INVALID_PHONE_NUM_LETTERS);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidLongPhoneNumber() {
+        PhoneNumber phoneNumber = new PhoneNumber(INVALID_PHONE_NUM_TOO_LONG);
+    }
+
+}
