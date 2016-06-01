79c8bc7430deec5cb6d489898bc44acb698c0b37
Eddie Marchioni <edwardm.deci@gmail.com>
2016-05-31 18:25:23 -0700

    Changes boolean successes to integer counts

--- src/main/java/ca/sfu/cmpt373/alpha/vrcladder/ladder/LadderTest.java
+++ src/main/java/ca/sfu/cmpt373/alpha/vrcladder/ladder/LadderTest.java
@@ -38,30 +38,338 @@ class LadderTest
 	public boolean RunTests()
 	{
 		
-		boolean Success = true;
+		int Successes = 0;
+		int MiniSuccesses = 0;
+		int TestResults = 0;
+
+		TestResults = TestCase1();
+		if(TestResults == 9)
+		{
+
+			System.out.println("Test Case 1 Passed, 9/9\n");
+			Successes++;
+
+		}
+		else
+		{
+
+			System.out.println("Test Case 1 Failed, " + TestResults + "/9\n");
+
+		}
+		MiniSuccesses += TestResults;
 		
-		Success = TestCase1() ? Success : false;
-		Success = TestCase2() ? Success : false;
-		Success = TestCase3() ? Success : false;
-		Success = TestCase4() ? Success : false;
-		Success = TestCase5() ? Success : false;
-		Success = TestCase6() ? Success : false;
-		Success = TestCase7() ? Success : false;
-		Success = TestCase8() ? Success : false;
-		Success = TestCase9() ? Success : false;
-		Success = TestCase10() ? Success : false;
-		Success = TestCase11() ? Success : false;
-		Success = TestCase12() ? Success : false;
-		Success = TestCase13() ? Success : false;
-		Success = TestCase14() ? Success : false;
-		Success = TestCase15() ? Success : false;
-		Success = TestCase16() ? Success : false;
-		Success = TestCase17() ? Success : false;
-		Success = TestCase18() ? Success : false;
-		Success = TestCase19() ? Success : false;
-		Success = TestCase20() ? Success : false;
+		TestResults = TestCase2();
+		if(TestResults == 9)
+		{
+
+			System.out.println("Test Case 2 Passed, 9/9\n");
+			Successes++;
+
+		}
+		else
+		{
+
+			System.out.println("Test Case 2 Failed, " + TestResults + "/9\n");
+
+		}
+		MiniSuccesses += TestResults;
+
+		TestResults = TestCase3();
+		if(TestResults == 9)
+		{
+
+			System.out.println("Test Case 3 Passed, 9/9\n");
+			Successes++;
+
+		}
+		else
+		{
+
+			System.out.println("Test Case 3 Failed, " + TestResults + "/9\n");
+
+		}
+		MiniSuccesses += TestResults;
+
+		TestResults = TestCase4();
+		if(TestResults == 9)
+		{
+
+			System.out.println("Test Case 4 Passed, 9/9\n");
+			Successes++;
+
+		}
+		else
+		{
+
+			System.out.println("Test Case 4 Failed, " + TestResults + "/9\n");
+
+		}
+		MiniSuccesses += TestResults;
+
+		TestResults = TestCase5();
+		if(TestResults == 9)
+		{
+
+			System.out.println("Test Case 5 Passed, 9/9\n");
+			Successes++;
+
+		}
+		else
+		{
+
+			System.out.println("Test Case 5 Failed, " + TestResults + "/9\n");
+
+		}
+		MiniSuccesses += TestResults;
+
+		TestResults = TestCase6();
+		if(TestResults == 9)
+		{
+
+			System.out.println("Test Case 6 Passed, 9/9\n");
+			Successes++;
+
+		}
+		else
+		{
+
+			System.out.println("Test Case 6 Failed, " + TestResults + "/9\n");
+
+		}
+		MiniSuccesses += TestResults;
+
+		TestResults = TestCase7();
+		if(TestResults == 9)
+		{
+
+			System.out.println("Test Case 7 Passed, 9/9\n");
+			Successes++;
+
+		}
+		else
+		{
+
+			System.out.println("Test Case 7 Failed, " + TestResults + "/9\n");
+
+		}
+		MiniSuccesses += TestResults;
+
+		TestResults = TestCase8();
+		if(TestResults == 9)
+		{
+
+			System.out.println("Test Case 8 Passed, 9/9\n");
+			Successes++;
+
+		}
+		else
+		{
+
+			System.out.println("Test Case 8 Failed, " + TestResults + "/9\n");
+
+		}
+		MiniSuccesses += TestResults;
+
+		TestResults = TestCase9();
+		if(TestResults == 9)
+		{
+
+			System.out.println("Test Case 9 Passed, 9/9\n");
+			Successes++;
+
+		}
+		else
+		{
+
+			System.out.println("Test Case 9 Failed, " + TestResults + "/9\n");
+
+		}
+		MiniSuccesses += TestResults;
+
+		TestResults = TestCase10();
+		if(TestResults == 9)
+		{
+
+			System.out.println("Test Case 10 Passed, 9/9\n");
+			Successes++;
+
+		}
+		else
+		{
+
+			System.out.println("Test Case 10 Failed, " + TestResults + "/9\n");
+
+		}
+		MiniSuccesses += TestResults;
+
+		TestResults = TestCase11();
+		if(TestResults == 9)
+		{
+
+			System.out.println("Test Case 11 Passed, 9/9\n");
+			Successes++;
+
+		}
+		else
+		{
+
+			System.out.println("Test Case 11 Failed, " + TestResults + "/9\n");
+
+		}
+		MiniSuccesses += TestResults;
 		
-		return Success;
+		TestResults = TestCase12();
+		if(TestResults == 9)
+		{
+
+			System.out.println("Test Case 12 Passed, 9/9\n");
+			Successes++;
+
+		}
+		else
+		{
+
+			System.out.println("Test Case 12 Failed, " + TestResults + "/9\n");
+
+		}
+		MiniSuccesses += TestResults;
+
+		TestResults = TestCase13();
+		if(TestResults == 9)
+		{
+
+			System.out.println("Test Case 13 Passed, 9/9\n");
+			Successes++;
+
+		}
+		else
+		{
+
+			System.out.println("Test Case 13 Failed, " + TestResults + "/9\n");
+
+		}
+		MiniSuccesses += TestResults;
+
+		TestResults = TestCase14();
+		if(TestResults == 9)
+		{
+
+			System.out.println("Test Case 14 Passed, 9/9\n");
+			Successes++;
+
+		}
+		else
+		{
+
+			System.out.println("Test Case 14 Failed, " + TestResults + "/9\n");
+
+		}
+		MiniSuccesses += TestResults;
+
+		TestResults = TestCase15();
+		if(TestResults == 9)
+		{
+
+			System.out.println("Test Case 15 Passed, 9/9\n");
+			Successes++;
+
+		}
+		else
+		{
+
+			System.out.println("Test Case 15 Failed, " + TestResults + "/9\n");
+
+		}
+		MiniSuccesses += TestResults;
+
+		TestResults = TestCase16();
+		if(TestResults == 9)
+		{
+
+			System.out.println("Test Case 16 Passed, 9/9\n");
+			Successes++;
+
+		}
+		else
+		{
+
+			System.out.println("Test Case 16 Failed, " + TestResults + "/9\n");
+
+		}
+		MiniSuccesses += TestResults;
+
+		TestResults = TestCase17();
+		if(TestResults == 9)
+		{
+
+			System.out.println("Test Case 17 Passed, 9/9\n");
+			Successes++;
+
+		}
+		else
+		{
+
+			System.out.println("Test Case 17 Failed, " + TestResults + "/9\n");
+
+		}
+		MiniSuccesses += TestResults;
+
+		TestResults = TestCase18();
+		if(TestResults == 9)
+		{
+
+			System.out.println("Test Case 18 Passed, 9/9\n");
+			Successes++;
+
+		}
+		else
+		{
+
+			System.out.println("Test Case 18 Failed, " + TestResults + "/9\n");
+
+		}
+		MiniSuccesses += TestResults;
+
+		TestResults = TestCase19();
+		if(TestResults == 9)
+		{
+
+			System.out.println("Test Case 19 Passed, 9/9\n");
+			Successes++;
+
+		}
+		else
+		{
+
+			System.out.println("Test Case 19 Failed, " + TestResults + "/9\n");
+
+		}
+		MiniSuccesses += TestResults;
+
+		TestResults = TestCase20();
+		if(TestResults == 9)
+		{
+
+			System.out.println("Test Case 20 Passed, 9/9\n");
+			Successes++;
+
+		}
+		else
+		{
+
+			System.out.println("Test Case 20 Failed, " + TestResults + "/9\n");
+
+		}
+		MiniSuccesses += TestResults;
+		
+		if(Successes == 20)
+		{
+
+			return true;
+
+		}
+
+		return false;
 		
 	}
 	
@@ -92,10 +400,10 @@ class LadderTest
 		
 	}
 	
-	private boolean TestCase1() //Case 1: Groups are directly beside one another, groups of 3 only
+	private int TestCase1() //Case 1: Groups are directly beside one another, groups of 3 only
 	{
 		
-		boolean Success = true;
+		int Successes = 0;
 		
 		System.out.println("Starting Test Case 1:");
 		
@@ -113,154 +421,156 @@ class LadderTest
 			
 			Ladder TestLadder = new Ladder(LadderTestTeams);
 			TestLadder.<somemethod>();
+
+			//check result
 			
 			if(SubSuccess)
 			{
 				
 				System.out.println("		Passed");
+				Successes++;
 				
 			}
 			else
 			{
 				
 				System.out.println("		Failed");
-				Success = false;
 				
 			}
 			
 		}
 		
-		return Success;
+		return Successes;
 		
 	}
 	
-	private boolean TestCase2() //Case 2: Groups are directly beside one another, groups of 4 only
+	private int TestCase2() //Case 2: Groups are directly beside one another, groups of 4 only
 	{
 		
 		
 		
 	}
 	
-	private boolean TestCase3() //Case 3: Groups are directly beside one another, groups of 3/4, not predictably interchanging (At least one of each)
-	{
-		
-		//here
-		
-	}
-	
-	private boolean TestCase4() //Case 4: Groups have non-attending teams between them, groups of 3 only
+	private int TestCase3() //Case 3: Groups are directly beside one another, groups of 3/4, not predictably interchanging (At least one of each)
 	{
 		
 		//here
 		
 	}
 	
-	private boolean TestCase5() //Case 5: Groups have non-attending teams between them, groups of 4 only
+	private int TestCase4() //Case 4: Groups have non-attending teams between them, groups of 3 only
 	{
 		
 		//here
 		
 	}
 	
-	private boolean TestCase6() //Case 6: Groups have non-attending teams between them, groups of 3/4, not predictably interchanging (At least one of each)
+	private int TestCase5() //Case 5: Groups have non-attending teams between them, groups of 4 only
 	{
 		
 		//here
 		
 	}
 	
-	private boolean TestCase7() //Case 7: Groups have non-attending teams inside them, groups of 3 only
+	private int TestCase6() //Case 6: Groups have non-attending teams between them, groups of 3/4, not predictably interchanging (At least one of each)
 	{
 		
 		//here
 		
 	}
 	
-	private boolean TestCase8() //Case 8: Groups have non-attending teams inside them, groups of 4 only
+	private int TestCase7() //Case 7: Groups have non-attending teams inside them, groups of 3 only
 	{
 		
 		//here
 		
 	}
 	
-	private boolean TestCase9() //Case 9: Groups have non-attending teams inside them, groups of 3/4, not predictably interchanging (At least one of each)
+	private int TestCase8() //Case 8: Groups have non-attending teams inside them, groups of 4 only
 	{
 		
 		//here
 		
 	}
 	
-	private boolean TestCase10() //Case 10: Single group of 3, no other teams
+	private int TestCase9() //Case 9: Groups have non-attending teams inside them, groups of 3/4, not predictably interchanging (At least one of each)
 	{
 		
 		//here
 		
 	}
 	
-	private boolean TestCase11() //Case 11: Single group of 3, other non-attending teams around
+	private int TestCase10() //Case 10: Single group of 3, no other teams
 	{
 		
 		//here
 		
 	}
 	
-	private boolean TestCase12() //Case 12: Single group of 4, no other teams
+	private int TestCase11() //Case 11: Single group of 3, other non-attending teams around
 	{
 		
 		//here
 		
 	}
 	
-	private boolean TestCase13() //Case 13: Single group of 4, other non-attending teams around
+	private int TestCase12() //Case 12: Single group of 4, no other teams
 	{
 		
 		//here
 		
 	}
 	
-	private boolean TestCase14() //Case 14: Single group of 3, all other non-attending teams after
+	private int TestCase13() //Case 13: Single group of 4, other non-attending teams around
 	{
 		
 		//here
 		
 	}
 	
-	private boolean TestCase15() //Case 15: Single group of 3, all other non-attending teams before
+	private int TestCase14() //Case 14: Single group of 3, all other non-attending teams after
 	{
 		
 		//here
 		
 	}
 	
-	private boolean TestCase16() //Case 16: Single group of 4, all other non-attending teams after
+	private int TestCase15() //Case 15: Single group of 3, all other non-attending teams before
 	{
 		
 		//here
 		
 	}
 	
-	private boolean TestCase17() //Case 17: Single group of 4, all other non-attending teams before
+	private int TestCase16() //Case 16: Single group of 4, all other non-attending teams after
 	{
 		
 		//here
 		
 	}
 	
-	private boolean TestCase18() //Case 18: Groups have non-attending teams beside and inside them, groups of 3 only, all overlapping
+	private int TestCase17() //Case 17: Single group of 4, all other non-attending teams before
 	{
 		
 		//here
 		
 	}
 	
-	private boolean TestCase19() //Case 19: Groups have non-attending teams beside and inside them, groups of 4 only, all overlapping
+	private int TestCase18() //Case 18: Groups have non-attending teams beside and inside them, groups of 3 only, all overlapping
 	{
 		
 		//here
 		
 	}
 	
-	private boolean TestCase20() //Case 20: Groups have non-attending teams beside and inside them, groups of 3/4, all overlapping, not predictably interchanging (At least one of each)
+	private int TestCase19() //Case 19: Groups have non-attending teams beside and inside them, groups of 4 only, all overlapping
+	{
+		
+		//here
+		
+	}
+	
+	private int TestCase20() //Case 20: Groups have non-attending teams beside and inside them, groups of 3/4, all overlapping, not predictably interchanging (At least one of each)
 	{
 		
 		//here
