433b9bd58f60c6559ee52a41d0354bf9a92c376e
Karan Sharma <ksa95@sfu.ca>
2016-06-01 19:43:46 -0700

    Create Decorator class for use in text UI

---
+++ src/main/java/ca/sfu/cmpt373/alpha/vrctextui/Decorator.java
@@ -0,0 +1,46 @@
+package ca.sfu.cmpt373.alpha.vrctextui;
+
+/**
+ * Prints text to console with decoration in the form of boxes and lines
+ */
+public class Decorator {
+    public static void printLine(int length, char decoration) {
+        while (length > 0) {
+            System.out.print(decoration);
+            length--;
+        }
+        System.out.print('\n');
+    }
+
+    public static void printInBox(final String[] textLines, char decoration) {
+        final int LATERAL_OFFSET = 4;
+        int maxLineLength = getMaxLineLength(textLines);
+        printLine(maxLineLength + LATERAL_OFFSET, decoration);
+        for (String line : textLines) {
+            String paddedText = "%c %-" + maxLineLength + "s %c\n";
+            System.out.printf(paddedText, decoration, line, decoration);
+        }
+        printLine(maxLineLength + LATERAL_OFFSET, decoration);
+    }
+
+    public static void printInBox(final String text, char decoration) {
+        printInBox(new String[]{text}, decoration);
+    }
+
+    private static int getMaxLineLength(final String[] lines) {
+        int maxLineLength = -1;
+        for (String line : lines) {
+            int currentLineLength = line.length();
+            if (currentLineLength > maxLineLength) {
+                maxLineLength = currentLineLength;
+            }
+        }
+        return maxLineLength;
+    }
+
+    public static void printAsNumberedList(final String[] textLines) {
+        for (int i = 0; i < textLines.length; i++) {
+            System.out.println((i + 1) + ". " + textLines[i]);
+        }
+    }
+}
--- src/main/java/ca/sfu/cmpt373/alpha/vrctextui/uimain.java
+++ src/main/java/ca/sfu/cmpt373/alpha/vrctextui/uimain.java
@@ -4,4 +4,13 @@ package ca.sfu.cmpt373.alpha.vrctextui;
  * Created by Hassan Fahad on 2016-06-01.
  */
 public class uimain {
+    public static void main(String[] args) {
+        String[] lines = {"demo of decorator", "class functionality", "for use in UI"};
+
+        Decorator.printInBox(lines, '*');
+
+        System.out.println();
+
+        Decorator.printAsNumberedList(lines);
+    }
 }
