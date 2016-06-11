ab041cf8c39577f18b3c3130c0c5d5ca051dbbe3
dquong <dquong@sfu.ca>
2016-06-10 19:10:01 -0700

    Refactor UserGenerator
    Refactor UserGenerator to be a static utility class, allowing generation
    of mock User's (players) for tests.

--- backend/src/test/java/ca/sfu/cmpt373/alpha/vrcladder/util/UserGenerator.java
+++ backend/src/test/java/ca/sfu/cmpt373/alpha/vrcladder/util/UserGenerator.java
@@ -1,77 +1,65 @@
-// TODO - Update this to account for changes in the User class
-// This file is commented because it does not compile.
-//
-//package ca.sfu.cmpt373.alpha.vrcladder.util;
-//
-//import ca.sfu.cmpt373.alpha.vrcladder.users.User;
-//import ca.sfu.cmpt373.alpha.vrcladder.users.authorization.UserRole;
-//import ca.sfu.cmpt373.alpha.vrcladder.users.personal.EmailAddress;
-//import ca.sfu.cmpt373.alpha.vrcladder.users.personal.PhoneNumber;
-//import ca.sfu.cmpt373.alpha.vrcladder.users.personal.UserId;
-//
-//import java.io.BufferedReader;
-//import java.io.FileNotFoundException;
-//import java.io.FileReader;
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Random;
-//import java.util.Scanner;
-//
-////import java.io.BufferedReader;
-//
-//public class UserGenerator {
-//
-//    final int MAX_ID = 99999999;
-//
-//
-//    final String[] AREA_CODE = new String[]{"604", "778"};
-//    final int PHONE_DIGITS = 10;
-//    final short EMAIL_STARTS_WITH = 0;
-//
-//    public UserGenerator() {
-//
-//    }
-//
-//    public User generateUser() {
-//        //get names file
-//        Scanner names;
-//        try {
-//            names = new Scanner(new BufferedReader(new FileReader("res\\names.txt")));
-//        } catch (FileNotFoundException e) {
-//            System.out.printf("%s", e.getMessage());
-//            e.printStackTrace();
-//            return null;
-//        }
-//
-//        List<String> nameList = new ArrayList<>();
-//        do {
-//            nameList.add(names.next());
-//        } while (names.hasNext());
-//
-//        //create player data
-//        Random randomInt = new Random();
-//        UserName username = new UserName(nameList.get(randomInt.nextInt(nameList.size())), null,
-//                nameList.get(randomInt.nextInt(nameList.size())));
-//        names.close();
-//        UserId userID = new UserId(randomInt.nextInt(MAX_ID));
-//
-//        String newEmail = username.getFirstName().charAt(EMAIL_STARTS_WITH) + username.getLastName() + "@vrc.ca";
-//        EmailAddress userEmail = new EmailAddress(newEmail);
-//
-//        //build a phone number
-//        StringBuilder phone = new StringBuilder();
-//        Random randomAreaCode = new Random();
-//        int rn = randomAreaCode.nextInt(AREA_CODE.length);
-//        phone.append(AREA_CODE[rn]);
-//        phone.append("-" + randomAreaCode.nextInt(PHONE_DIGITS) + randomAreaCode.nextInt(PHONE_DIGITS)
-//                + randomAreaCode.nextInt(PHONE_DIGITS));
-//        phone.append("-" + randomAreaCode.nextInt(PHONE_DIGITS) + randomAreaCode.nextInt(PHONE_DIGITS)
-//                + randomAreaCode.nextInt(PHONE_DIGITS) + randomAreaCode.nextInt(PHONE_DIGITS));
-//        PhoneNumber userPhone = new PhoneNumber(phone.toString());
-//
-//        User user = new User(userID, UserRole.PLAYER, username, userEmail, userPhone);
-//
-//
-//        return user;
-//    }
-//}
+package ca.sfu.cmpt373.alpha.vrcladder.util;
+
+import ca.sfu.cmpt373.alpha.vrcladder.users.User;
+import ca.sfu.cmpt373.alpha.vrcladder.users.UserBuilder;
+import ca.sfu.cmpt373.alpha.vrcladder.users.authorization.UserRole;
+import ca.sfu.cmpt373.alpha.vrcladder.users.personal.EmailAddress;
+import ca.sfu.cmpt373.alpha.vrcladder.users.personal.PhoneNumber;
+import ca.sfu.cmpt373.alpha.vrcladder.users.personal.UserId;
+import org.apache.commons.lang3.StringUtils;
+
+public class UserGenerator {
+
+    private final static String EMAIL_FORMAT = "user%s@vrc.ca";
+    private final static int PHONE_NUMBER_SIZE = 10;
+    private final static char PHONE_NUMBER_PADDING_CHAR = '0';
+
+    private static int userCount;
+
+    static {
+        userCount = 0;
+    }
+
+    public static User generatePlayer() {
+        userCount++;
+
+        String userCountStr = String.valueOf(userCount);
+
+        UserId userId = new UserId(userCountStr);
+        String firstName = "firstName" + userId;
+        String lastName = "lastName" + userId;
+        EmailAddress emailAddress = generateEmailAddress(userCountStr);
+        PhoneNumber phoneNumber = generatePhoneNumber(userCountStr);
+
+        User newUser = new UserBuilder()
+            .setUserId(userId)
+            .setUserRole(UserRole.PLAYER)
+            .setFirstName(firstName)
+            .setMiddleName(StringUtils.EMPTY)
+            .setLastName(lastName)
+            .setEmailAddress(emailAddress)
+            .setPhoneNumber(phoneNumber)
+            .buildUser();
+
+        return newUser;
+    }
+
+    private static EmailAddress generateEmailAddress(String userNum) {
+        String emailAddress = String.format(EMAIL_FORMAT, userNum);
+
+        return new EmailAddress(emailAddress);
+    }
+
+    private static PhoneNumber generatePhoneNumber(String userNum) {
+        int digitsNeeded = PHONE_NUMBER_SIZE - userNum.length();
+
+        StringBuilder numberPadder = new StringBuilder(userNum);
+        for (int i = 0; i < digitsNeeded; i++) {
+            numberPadder.insert(0, PHONE_NUMBER_PADDING_CHAR);
+        }
+
+        return new PhoneNumber(numberPadder.toString());
+    }
+
+}
+
