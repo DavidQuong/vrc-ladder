f97a106dc5fac785ac385c6c4e7edcbbbaa2139d
dquong <dquong@sfu.ca>
2016-06-23 16:10:56 -0700

    Implement ability to read/update play time
    - Implement REST API routes for reading and updating a team's preferred
    PlayTime, this can be achieved by a GET or PUT request to
    /team/<id>/attendance/playtime respectively.
    - Create NewPlayTimePayload which is used when updating the preferred
      PlayTime of a Team, during deserialization of the request body.

--- backend/src/main/java/ca/sfu/cmpt373/alpha/vrcrest/routes/TeamRouter.java
+++ backend/src/main/java/ca/sfu/cmpt373/alpha/vrcrest/routes/TeamRouter.java
@@ -5,16 +5,18 @@ import ca.sfu.cmpt373.alpha.vrcladder.exceptions.EntityNotFoundException;
 import ca.sfu.cmpt373.alpha.vrcladder.exceptions.ExistingTeamException;
 import ca.sfu.cmpt373.alpha.vrcladder.teams.Team;
 import ca.sfu.cmpt373.alpha.vrcladder.teams.TeamManager;
+import ca.sfu.cmpt373.alpha.vrcladder.teams.attendance.AttendanceCard;
 import ca.sfu.cmpt373.alpha.vrcladder.util.GeneratedId;
-import ca.sfu.cmpt373.alpha.vrcrest.datatransfer.requests.TeamSerializer;
-import ca.sfu.cmpt373.alpha.vrcrest.datatransfer.responses.NewTeamPayload;
+import ca.sfu.cmpt373.alpha.vrcrest.datatransfer.requests.NewPlayTimePayload;
+import ca.sfu.cmpt373.alpha.vrcrest.datatransfer.requests.NewTeamPayload;
+import ca.sfu.cmpt373.alpha.vrcrest.datatransfer.responses.AttendanceCardGsonSerializer;
+import ca.sfu.cmpt373.alpha.vrcrest.datatransfer.responses.TeamGsonSerializer;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonSyntaxException;
-import com.google.gson.stream.MalformedJsonException;
 import org.eclipse.jetty.http.HttpStatus;
 import org.hibernate.exception.ConstraintViolationException;
 import spark.Request;
@@ -23,15 +25,18 @@ import spark.Spark;
 
 import java.util.List;
 
-// TODO - Implement private route methods.
+// TODO - Implement updating a team's players
 public class TeamRouter extends RestRouter {
 
     public static final String ROUTE_TEAMS = "/teams";
     public static final String ROUTE_TEAM_ID = "/team/" + ROUTE_ID;
     public static final String ROUTE_TEAM_ID_ATTENDANCE = "/team/" + ROUTE_ID + "/attendance";
+    public static final String ROUTE_TEAM_ID_ATTENDANCE_PLAYTIME = ROUTE_TEAM_ID_ATTENDANCE + "/playtime";
+    public static final String ROUTE_TEAM_ID_ATTENDANCE_STATUS = ROUTE_TEAM_ID_ATTENDANCE + "/status";
 
     public static final String JSON_PROPERTY_TEAMS = "teams";
     public static final String JSON_PROPERTY_TEAM = "team";
+    public static final String JSON_PROPERTY_ATTENDANCE = "attendance";
 
     private static final String ERROR_PLAYER_ID_NOT_FOUND = "One of the provided player ID's cannot be found.";
     private static final String ERROR_EXISTING_TEAM = "The provided pair of player's already form a team.";
@@ -53,14 +58,19 @@ public class TeamRouter extends RestRouter {
         Spark.get(ROUTE_TEAM_ID, (request, response) -> handleUpdateTeamById(request, response));
         Spark.delete(ROUTE_TEAM_ID, (request, response) -> handleDeleteTeamById(request, response));
         Spark.get(ROUTE_TEAM_ID_ATTENDANCE, (request, response) -> handleGetTeamAttendance(request, response));
-        Spark.put(ROUTE_TEAM_ID_ATTENDANCE, (request, response) -> handleUpdateGetAttendance(request, response));
+        Spark.put(ROUTE_TEAM_ID_ATTENDANCE_PLAYTIME, (request, response) -> handleUpdatePlayTime(request, response));
+        Spark.put(ROUTE_TEAM_ID_ATTENDANCE_STATUS, (request, response) -> handleUpdateAttendanceStatus(request,
+            response));
+
     }
 
     @Override
     protected Gson buildGson() {
         return new GsonBuilder()
-            .registerTypeAdapter(Team.class, new TeamSerializer())
+            .registerTypeAdapter(Team.class, new TeamGsonSerializer())
+            .registerTypeAdapter(AttendanceCard.class, new AttendanceCardGsonSerializer())
             .registerTypeAdapter(NewTeamPayload.class, new NewTeamPayload.GsonDeserializer())
+            .registerTypeAdapter(NewPlayTimePayload.class, new NewPlayTimePayload.GsonDeserializer())
             .setPrettyPrinting()
             .create();
     }
@@ -144,6 +154,7 @@ public class TeamRouter extends RestRouter {
         return responseBody.toString();
     }
 
+    // TODO - Enable updating the players on a Team.
     private String handleUpdateTeamById(Request request, Response response) {
         return null;
     }
@@ -173,10 +184,66 @@ public class TeamRouter extends RestRouter {
     }
 
     private String handleGetTeamAttendance(Request request, Response response) {
-        return null;
+        JsonObject responseBody = new JsonObject();
+
+        String paramId = request.params(ROUTE_ID);
+        GeneratedId generatedId = new GeneratedId(paramId);
+
+        try {
+            Team existingTeam = teamManager.getById(generatedId);
+            AttendanceCard attendanceCard = existingTeam.getAttendanceCard();
+
+            responseBody.add(JSON_PROPERTY_ATTENDANCE, getGson().toJsonTree(attendanceCard));
+            response.status(HttpStatus.OK_200);
+        } catch (EntityNotFoundException ex) {
+            responseBody.addProperty(JSON_PROPERTY_ERROR, ERROR_NONEXISTENT_TEAM);
+            response.status(HttpStatus.NOT_FOUND_404);
+
+        } catch (RuntimeException ex) {
+            responseBody.addProperty(JSON_PROPERTY_ERROR, ERROR_COULD_NOT_COMPLETE_REQUEST);
+            response.status(HttpStatus.BAD_REQUEST_400);
+        }
+
+        response.type(JSON_RESPONSE_TYPE);
+        return responseBody.toString();
     }
 
-    private String handleUpdateGetAttendance(Request request, Response response) {
+    private String handleUpdatePlayTime(Request request, Response response) {
+        JsonObject responseBody = new JsonObject();
+
+        String paramId = request.params(ROUTE_ID);
+        GeneratedId generatedId = new GeneratedId(paramId);
+
+        try {
+            NewPlayTimePayload playTimePayload = getGson().fromJson(request.body(), NewPlayTimePayload.class);
+            Team existingTeam = teamManager.updateAttendance(generatedId, playTimePayload.getPlayTime());
+            AttendanceCard attendanceCard = existingTeam.getAttendanceCard();
+
+            responseBody.add(JSON_PROPERTY_ATTENDANCE, getGson().toJsonTree(attendanceCard));
+            response.status(HttpStatus.OK_200);
+        } catch (JsonSyntaxException ex) {
+            responseBody.addProperty(JSON_PROPERTY_ERROR, ERROR_MALFORMED_JSON);
+            response.status(HttpStatus.BAD_REQUEST_400);
+
+        } catch (JsonParseException | IllegalArgumentException ex) {
+            responseBody.addProperty(JSON_PROPERTY_ERROR, ex.getMessage());
+            response.status(HttpStatus.UNPROCESSABLE_ENTITY_422);
+
+        } catch (EntityNotFoundException ex) {
+            responseBody.addProperty(JSON_PROPERTY_ERROR, ERROR_NONEXISTENT_TEAM);
+            response.status(HttpStatus.NOT_FOUND_404);
+
+        } catch (RuntimeException ex) {
+            responseBody.addProperty(JSON_PROPERTY_ERROR, ERROR_COULD_NOT_COMPLETE_REQUEST);
+            response.status(HttpStatus.BAD_REQUEST_400);
+        }
+
+        response.type(JSON_RESPONSE_TYPE);
+        return responseBody.toString();
+    }
+
+    // TODO - Enable updating a Team's AttendanceStatus.
+    private String handleUpdateAttendanceStatus(Request request, Response response) {
         return null;
     }
 
