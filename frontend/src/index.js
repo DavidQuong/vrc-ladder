50c1e64a1704ebf7c9103b3b19d989aec14c132c
Aman Sandhu <ass17@sfu.ca>
2016-06-25 22:33:59 -0700

    Separate Pages.
    Using react-router the components are now on separate pages with links
    connecting them. React-Redux dev tools do not work very well with
    the router, causing the history slider and the objects to reset
    anytime you switch pages.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,37 @@
 import {createElement, Element} from 'react';
 import {Provider} from 'react-redux';
 import {IntlProvider} from 'react-intl';
-import App from './app';
+import {Router, Route, IndexRoute, browserHistory, Link} from 'react-router';
+import SignUp from './component/signup/signup';
+import Ladder from './component/ladder/ladder';
+import CreateTeam from './component/create-team/create-team';
+
+const Layout = ({children}) => (
+  <div>
+    <div>
+      <Link to='/signup'>Sign Up</Link>
+    </div>
+    <div>
+      <Link to='/ladder'>Ladder</Link>
+    </div>
+    <div>
+      <Link to='/create-team'>Create team</Link>
+    </div>
+    {children}
+  </div>
+);
 
 export default ({store}) : Element => (
   <Provider store={store}>
     <IntlProvider messages={{}} defaultLocale='en-US'>
-      <App/>
+      <Router history={browserHistory}>
+        <Route path='/' component={Layout}>
+          <IndexRoute component={() => <div>INDEX</div>}/>
+          <Route path='/signup' component={SignUp}/>
+          <Route path='/ladder' component={Ladder}/>
+          <Route path='/create-team' component={CreateTeam}/>
+        </Route>
+      </Router>
     </IntlProvider>
   </Provider>
 );
