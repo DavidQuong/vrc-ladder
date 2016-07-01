ba9403815f759ea7eeadf295ea3b30d38eae9bbf
dquong <dquong@sfu.ca>
2016-07-01 01:47:24 -0700

    Refactor RestRouter error handling
    Throw an AuthorizationException within the extractAuthorizationToken
    method in the RestRouter class instead of immediately halting
    unauthorized requests.

--- backend/src/main/java/ca/sfu/cmpt373/alpha/vrcrest/routes/RestRouter.java
+++ backend/src/main/java/ca/sfu/cmpt373/alpha/vrcrest/routes/RestRouter.java
@@ -1,15 +1,10 @@
 package ca.sfu.cmpt373.alpha.vrcrest.routes;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import org.apache.shiro.authz.AuthorizationException;
-import org.eclipse.jetty.http.HttpStatus;
 import spark.Request;
-import spark.Response;
-import spark.Spark;
 
 public abstract class RestRouter {
-
 
     public static final String PARAM_ID = ":id";
 
@@ -19,9 +14,12 @@ public abstract class RestRouter {
     public static final String JSON_RESPONSE_TYPE = "application/json";
     public static final String JSON_PROPERTY_ERROR = "error";
 
-    protected static final String ERROR_INVALID_AUTHORIZATION = "Invalid authorization, could not perform request.";
     protected static final String ERROR_COULD_NOT_COMPLETE_REQUEST = "Request could not be completed.";
     protected static final String ERROR_MALFORMED_JSON = "The provided JSON in the request body is malformed.";
+
+    private static final String ERROR_MISSING_AUTHORIZATION_TOKEN = "The request is missing the Authorization header.";
+    private static final String ERROR_INVALID_AUTHORIZATION_FORMAT = "The Authorization header should be in the " +
+        "format: Bearer <token>.";
 
     private static final int EXPECTED_AUTHORIZATION_HEADER_WORD_COUNT = 2;
     private static final int AUTHORIZATION_BEARER_INDEX = 0;
@@ -37,30 +35,22 @@ public abstract class RestRouter {
         return gson;
     }
 
-    protected String extractAuthorizationToken(Request request, Response response) {
+    protected String extractAuthorizationToken(Request request) {
         String authorizationHeader = request.headers(HEADER_AUTHORIZATION);
         if (authorizationHeader == null) {
-            haltUnauthorizedRequest(response);
+            throw new AuthorizationException(ERROR_MISSING_AUTHORIZATION_TOKEN);
         }
 
         String[] authorizationHeaderWords = authorizationHeader.split(" ");
         if (authorizationHeaderWords.length != EXPECTED_AUTHORIZATION_HEADER_WORD_COUNT) {
-            throw new AuthorizationException();
+            throw new AuthorizationException(ERROR_INVALID_AUTHORIZATION_FORMAT);
         }
 
         if (!authorizationHeaderWords[AUTHORIZATION_BEARER_INDEX].equals(HEADER_AUTHORIZATION_VALUE_BEARER)) {
-            throw new AuthorizationException();
+            throw new AuthorizationException(ERROR_INVALID_AUTHORIZATION_FORMAT);
         }
 
         return authorizationHeaderWords[AUTHORIZATION_TOKEN_INDEX];
-    }
-
-    private void haltUnauthorizedRequest(Response response) {
-        JsonObject responseBody = new JsonObject();
-        responseBody.addProperty(JSON_PROPERTY_ERROR, ERROR_INVALID_AUTHORIZATION);
-
-        response.type(JSON_RESPONSE_TYPE);
-        Spark.halt(HttpStatus.UNAUTHORIZED_401, responseBody.toString());
     }
 
     public abstract void attachRoutes();
