e264ef798262b57af5be9d62d925ca21ae85d8ae
Karan Sharma <ksa95@sfu.ca>
2016-07-01 05:08:39 -0700

    Implement unstyled navbar with logo
    - Supports a mobile-friendly display with toggle dropdown
    - Import Lato Google font
    - Change title of web app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,23 +5,38 @@ import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import SignUp from './component/signup/signup';
 import Ladder from './component/ladder/ladder';
 import CreateTeam from './component/create-team/create-team';
-import {Button, ButtonToolbar} from 'react-bootstrap';
+import {Nav, NavItem, Navbar} from 'react-bootstrap';
 import {LinkContainer} from 'react-router-bootstrap';
 
 const Layout = ({children}) => (
   <div>
     <div>
-      <ButtonToolbar>
-        <LinkContainer to='/signup'>
-          <Button>Sign up</Button>
-        </LinkContainer>
-        <LinkContainer to='/ladder'>
-          <Button>Ladder</Button>
-        </LinkContainer>
-        <LinkContainer to='/create-team'>
-          <Button>Create team</Button>
-        </LinkContainer>
-      </ButtonToolbar>
+      <Navbar inverse>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <LinkContainer to='/'>
+              <img src={require('url?limit=10000!./../src/public/logo.png')} />
+            </LinkContainer>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Navbar.Brand>
+            VRC RACQUETS CLUB
+          </Navbar.Brand>
+          <Nav pullRight>
+            <LinkContainer to='/ladder'>
+              <NavItem>Ladder</NavItem>
+            </LinkContainer>
+            <LinkContainer to='/create-team'>
+              <NavItem>Create team</NavItem>
+            </LinkContainer>
+            <LinkContainer to='/signup'>
+              <NavItem>Sign up</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
     <div>
       {children}
