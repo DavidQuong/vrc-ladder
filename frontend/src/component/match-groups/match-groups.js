23b1bec2f6fb576934a66dad44fc34ee7dd277ef
htusiime <htusiime@sfu.ca>
2016-07-02 23:41:32 -0700

    completed styling divs for matchgroups (lacks interactivity; to be implemented in Iteration 3)

--- frontend/src/component/match-groups/match-groups.js
+++ frontend/src/component/match-groups/match-groups.js
@@ -17,68 +17,163 @@ const MatchGroup = ({
     <div>
       <br/>
       <div className={styles.matchGroup}>
-        <div>
+        <div className={styles.matchGroupCourt}>
           <FormattedMessage
             id='court #1'
             defaultMessage='Court #1'
           />
         </div>
-          <FormattedMessage
-            id='match1'
-            defaultMessage='Team 1 vs Team 2 @8:30pm --> Winner Results: 1'
-          /><br/>
-          <FormattedMessage
-            id='match2'
-            defaultMessage='Team 1 vs Team 3 @8:30pm --> Winner Results: 1'
-          /><br/>
-          <FormattedMessage
-            id='match3'
-            defaultMessage='Team 2 vs Team 3 @8:30pm --> Winner Results: 3'
-          /><br/>
+          <div className={styles.matchGroupInfo}>
+            <FormattedMessage
+              id='match1'
+              defaultMessage='Team 1 vs Team 2 @8:30pm'
+            />
+            <div className={styles.matchGroupResultsFORM}>
+              <FormattedMessage
+                id='matchResults3'
+                defaultMessage='Results: <User enter results here (txt, drop?)>'
+              />
+              </div>
+            </div>
+            <br/>
+          <div className={styles.matchGroupInfo}>
+            <FormattedMessage
+              id='match2'
+              defaultMessage='Team 1 vs Team 3 @8:30pm'
+            />
+              <div className={styles.matchGroupResultsFORM}>
+              <FormattedMessage
+                id='matchResults3'
+                defaultMessage='Results: <User enter results here (txt, drop?)>'
+              />
+              </div>
+            </div>
+          <br/>
+            <div className={styles.matchGroupInfo}>
+            <FormattedMessage
+              id='matchResults3'
+              defaultMessage='Team 2 vs Team 3 @8:30pm'
+            />
+              <div className={styles.matchGroupResultsFORM}>
+              <FormattedMessage
+                id='matchResults3'
+                defaultMessage='Results: <User enter results here (txt, drop?)>'
+              />
+              </div>
+            </div>
+          <br/>
       </div>
       <div className={styles.matchGroup}>
-        <div>
+        <div className={styles.matchGroupCourt}>
+          <FormattedMessage
+            id='matchGroup2'
+            defaultMessage='Court #2'
+          />
         </div>
-        <FormattedMessage
-          id='matchGroup2'
-          defaultMessage='MatchGroup 2'
-        /><br/>
-          <FormattedMessage
-            id='match1'
-            defaultMessage='Team 4 vs Team 5 @8:30pm -->    Winner Results:   4'
-          /><br/>
-          <FormattedMessage
-            id='match2'
-            defaultMessage='Team 4 vs Team 6 @8:30pm -->    Winner Results:   6'
-          /><br/>
-          <FormattedMessage
-            id='match3'
-            defaultMessage='Team 5 vs Team 6 @8:30pm -->    Winner Results:   4'
-          /><br/>
+          <div>
+            <div className={styles.matchGroupInfo}>
+            <FormattedMessage
+              id='match1'
+              defaultMessage='Team 4 vs Team 5 @8:30pm'
+            />
+            <div className={styles.matchGroupResultsFORM}>
+              <FormattedMessage
+                id='matchResults3'
+                defaultMessage='Results: <User enter results here (txt, drop?)>'
+              />
+              </div>
+            </div>
+            <br/>
+            <div className={styles.matchGroupInfo}>
+            <FormattedMessage
+              id='match2'
+              defaultMessage='Team 4 vs Team 6 @8:30pm'
+            />
+              <div className={styles.matchGroupResultsFORM}>
+              <FormattedMessage
+                id='matchResults3'
+                defaultMessage='Results: <User enter results here (txt, drop?)>'
+              />
+              </div>
+            </div>
+            <br/>
+            <div className={styles.matchGroupInfo}>
+              <FormattedMessage
+                id='match3'
+                defaultMessage='Team 5 vs Team 6 @8:30pm'
+              />
+              <div className={styles.matchGroupResultsFORM}>
+              <FormattedMessage
+                id='matchResults3'
+                defaultMessage='Results: <User enter results here (txt, drop?)>'
+              />
+              </div>
+            </div>
+          </div>
+          <br/>
       </div>
       <div className={styles.matchGroup}>
-        <div>
+        <div className={styles.matchGroupCourt}>
+          <FormattedMessage
+            id='matchGroup2'
+            defaultMessage='Court #2'
+          />
         </div>
-        <FormattedMessage
-          id='matchGroup3'
-          defaultMessage='MatchGroup 3'
-        /><br/>
-          <FormattedMessage
-            id='match1'
-            defaultMessage='Team 7 vs Team 8 @8:30pm -->    Winner Results:   7'
-          /><br/>
-          <FormattedMessage
-            id='match2'
-            defaultMessage='Team 7 vs Team 9 @8:30pm -->    Winner Results:   7'
-          /><br/>
-          <FormattedMessage
-            id='match3'
-            defaultMessage='Team 9 vs Team W @8:30pm -->    Winner Results:   9'
-          /><br/>
-          <FormattedMessage
-            id='match3'
-            defaultMessage='Team 10 vs Team D @8:30pm -->    Winner Results:  ~'
-          /><br/>
+          <div>
+            <div className={styles.matchGroupInfo}>
+            <FormattedMessage
+              id='match1'
+              defaultMessage='Team 7 vs Team 8 @9pm'
+            />
+            <div className={styles.matchGroupResultsFORM}>
+              <FormattedMessage
+                id='matchResults3'
+                defaultMessage='Results: <User enter results here (txt, drop?)>'
+              />
+              </div>
+            </div>
+            <br/>
+            <div className={styles.matchGroupInfo}>
+            <FormattedMessage
+              id='match2'
+              defaultMessage='Team 7 vs Team 9 @9pm'
+            />
+              <div className={styles.matchGroupResultsFORM}>
+              <FormattedMessage
+                id='matchResults3'
+                defaultMessage='Results: <User enter results here (txt, drop?)>'
+              />
+              </div>
+            </div>
+            <br/>
+            <div className={styles.matchGroupInfo}>
+              <FormattedMessage
+                id='match3'
+                defaultMessage='Team 9 vs Team W @9pm'
+              />
+              <div className={styles.matchGroupResultsFORM}>
+              <FormattedMessage
+                id='matchResults3'
+                defaultMessage='Results: <User enter results here (txt, drop?)>'
+              />
+              </div>
+            </div>
+            <br/>
+            <div className={styles.matchGroupInfo}>
+              <FormattedMessage
+                id='match4'
+                defaultMessage='Team 10 vs Team D @9pm'
+              />
+              <div className={styles.matchGroupResultsFORM}>
+              <FormattedMessage
+                id='matchResults4'
+                defaultMessage='Results: <User enter results here (txt, drop?)>'
+              />
+              </div>
+            </div>
+            <br/>
+          </div>
+          <br/>
       </div>
     </div>
   </div>
