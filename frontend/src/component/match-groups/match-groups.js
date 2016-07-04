26157bf1d23cf9a8082a6eb96182e289c997eb34
htusiime <htusiime@sfu.ca>
2016-07-04 00:28:29 -0700

    added team and player names (examples) for each court

--- frontend/src/component/match-groups/match-groups.js
+++ frontend/src/component/match-groups/match-groups.js
@@ -22,6 +22,29 @@ const MatchGroup = ({
             id='court #1'
             defaultMessage='Court #1'
           />
+          <br/>
+          <div className={styles.matchGroupCourtTeams}>
+            <div>
+              <FormattedMessage
+                id='team1'
+                defaultMessage='Team 1: Karan, Aman'
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                id='team2'
+                defaultMessage='Team 2: Trevor, David'
+              />
+            </div>
+          </div>
+          <div className={styles.matchGroupCourtTeams}>
+            <div>
+              <FormattedMessage
+                id='team3'
+                defaultMessage='Team 3: Karan, Aman '
+              />
+            </div>
+          </div>
         </div>
           <div className={styles.matchGroupInfo}>
             <FormattedMessage
@@ -66,6 +89,29 @@ const MatchGroup = ({
             id='matchGroup2'
             defaultMessage='Court #2'
           />
+          <br/>
+          <div className={styles.matchGroupCourtTeams}>
+            <div>
+              <FormattedMessage
+                id='team1'
+                defaultMessage='Team 4: Marcus, Marie'
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                id='team2'
+                defaultMessage='Team 5: Will, Danica'
+              />
+            </div>
+          </div>
+          <div className={styles.matchGroupCourtTeams}>
+            <div>
+              <FormattedMessage
+                id='team3'
+                defaultMessage='Team 6: Margerie, Nichols '
+              />
+            </div>
+          </div>
         </div>
           <div>
             <div className={styles.matchGroupInfo}>
@@ -110,14 +156,43 @@ const MatchGroup = ({
         <div className={styles.matchGroupCourt}>
           <FormattedMessage
             id='matchGroup2'
-            defaultMessage='Court #2'
+            defaultMessage='Court #3'
           />
+          <br/>
+          <div className={styles.matchGroupCourtTeams}>
+            <div>
+              <FormattedMessage
+                id='team1'
+                defaultMessage='Team 7: Marcus, Marie'
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                id='team2'
+                defaultMessage='Team 8: Will, Danica'
+              />
+            </div>
+          </div>
+          <div className={styles.matchGroupCourtTeams}>
+            <div>
+              <FormattedMessage
+                id='team3'
+                defaultMessage='Team 9: Margerie, Nichols '
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                id='team3'
+                defaultMessage='Team 10: Margerie, Nichols '
+              />
+            </div>
+          </div>
         </div>
           <div>
             <div className={styles.matchGroupInfo}>
             <FormattedMessage
               id='match1'
-              defaultMessage='Team 7 vs Team 8 @9pm'
+              defaultMessage='Team 7 vs Team 8 @9:00pm'
             />
             <div className={styles.matchGroupResultsFORM}>
               <FormattedMessage
@@ -129,7 +204,7 @@ const MatchGroup = ({
             <div className={styles.matchGroupInfo}>
             <FormattedMessage
               id='match2'
-              defaultMessage='Team 7 vs Team 9 @9pm'
+              defaultMessage='Team 7 vs Team 9 @9:00pm'
             />
               <div className={styles.matchGroupResultsFORM}>
               <FormattedMessage
@@ -141,7 +216,7 @@ const MatchGroup = ({
             <div className={styles.matchGroupInfo}>
               <FormattedMessage
                 id='match3'
-                defaultMessage='Team 9 vs Team W @9pm'
+                defaultMessage='Team 9 vs Team W @9:00pm'
               />
               <div className={styles.matchGroupResultsFORM}>
               <FormattedMessage
@@ -153,7 +228,7 @@ const MatchGroup = ({
             <div className={styles.matchGroupInfo}>
               <FormattedMessage
                 id='match4'
-                defaultMessage='Team 10 vs Team D @9pm'
+                defaultMessage='Team 10 vs Team D @9:00pm'
               />
               <div className={styles.matchGroupResultsFORM}>
               <FormattedMessage
