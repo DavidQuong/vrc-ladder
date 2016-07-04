145e4bed5e5648f17537be8f5d24f1e830a7c5c7
Karan Sharma <ksa95@sfu.ca>
2016-07-04 06:39:21 -0700

    Partially style team-create view

--- frontend/src/component/create-team/create-team.js
+++ frontend/src/component/create-team/create-team.js
@@ -3,7 +3,7 @@ import {FormattedMessage} from 'react-intl';
 import {connect} from 'react-redux';
 import {reduxForm} from 'redux-form';
 import {addTeam} from '../../action/teams';
-import {SubmitBtn} from '../button';
+// import {SubmitBtn} from '../button';
 import {withRouter} from 'react-router';
 
 import map from 'lodash/fp/map';
@@ -12,6 +12,7 @@ import Heading from '../heading/heading';
 import classNames from 'classnames';
 // import findIndex from 'lodash/fp/findIndex';
 import sortBy from 'lodash/fp/sortBy';
+import {Form, Well, Button} from 'react-bootstrap';
 
 const validate = (values) => {
   const errors = {};
@@ -36,69 +37,68 @@ const CreateTeamForm = reduxForm({
   players,
   handleSubmit,
 }) => (
-  <form
-    className={styles.formHorizontal}
-    onSubmit={handleSubmit}
-  >
-  <div className={classNames(styles.formGroup)}>
-    <label
-      className={classNames(styles.colXsTitle)}
-    >
-        <FormattedMessage
-          id='firstPlayerId'
-          defaultMessage='Select Team Members Name'
-        />
-      </label>
-      <select
-        className={classNames(styles.goodForm, {
-          [styles.errorForm]: secondPlayerId.error &&
-                              firstPlayerId.touched &&
-                              secondPlayerId.touched})}
-        {...firstPlayerId}
-      >
-        <option value=''>Select a player...</option>
-        {map((player) => (
-          <option value={player.userId}key={player.userId}>
-            {player.firstName} {player.userId}
-          </option>
-        ), players)}
-      </select>
+  <Form horizontal onSubmit={handleSubmit}>
+    <div>
+      <div className={classNames(styles.formGroup)}>
+        <label
+          className={classNames(styles.colXsTitle)}
+        >
+            <FormattedMessage
+              id='firstPlayerId'
+              defaultMessage='Select First Team Member'
+            />
+          </label>
+          <select
+            className={classNames(styles.goodForm, {
+              [styles.errorForm]: secondPlayerId.error &&
+                                  firstPlayerId.touched &&
+                                  secondPlayerId.touched})}
+            {...firstPlayerId}
+          >
+            <option value=''>Select a player...</option>
+            {map((player) => (
+              <option value={player.userId}key={player.userId}>
+                {player.firstName} {player.userId}
+              </option>
+            ), players)}
+          </select>
+        </div>
+        <div className={classNames(styles.formGroup)}>
+          <label
+            className={classNames(styles.colXsTitle)}
+          >
+            <FormattedMessage
+              id='secondPlayerId'
+              defaultMessage='Select Second Team Member'
+            />
+          </label>
+          <select
+            className={classNames(styles.goodForm, {
+              [styles.errorForm]: secondPlayerId.error &&
+                                  firstPlayerId.touched &&
+                                  secondPlayerId.touched})}
+            {...secondPlayerId}
+          >
+            <option value=''>Select a player...</option>
+            {map((player) => (
+              <option value={player.userId}key={player.userId}>
+                {player.firstName} {player.userId}
+              </option>
+            ), players)}
+          </select>
+          {firstPlayerId.touched && secondPlayerId.touched
+            && secondPlayerId.error &&
+            <div className={classNames(styles.errorMsg)}>
+              <Heading kind='error'>
+                {secondPlayerId.error}
+              </Heading>
+            </div>}
+        </div>
+        <div className={styles.center}>
+          <Button bsStyle='primary' bsSize='large' type='submit'>Log In</Button>
+        </div>
     </div>
-    <div className={classNames(styles.formGroup)}>
-      <label
-        className={classNames(styles.colXsTitle)}
-      >
-        <FormattedMessage
-          id='secondPlayerId'
-          defaultMessage='Select Team Members Name'
-        />
-      </label>
-      <select
-        className={classNames(styles.goodForm, {
-          [styles.errorForm]: secondPlayerId.error &&
-                              firstPlayerId.touched &&
-                              secondPlayerId.touched})}
-        {...secondPlayerId}
-      >
-        <option value=''>Select a player...</option>
-        {map((player) => (
-          <option value={player.userId}key={player.userId}>
-            {player.firstName} {player.userId}
-          </option>
-        ), players)}
-      </select>
-      {firstPlayerId.touched && secondPlayerId.touched
-        && secondPlayerId.error &&
-        <div className={classNames(styles.errorMsg)}>
-          <Heading kind='error'>
-            {secondPlayerId.error}
-          </Heading>
-        </div>}
-    </div>
-    <div className={classNames(styles.center)}>
-      <SubmitBtn type='submit'>Create Team</SubmitBtn>
-    </div>
-  </form>
+  </Form>
 ));
 
 const CreateTeam = withRouter(({
@@ -108,27 +108,29 @@ const CreateTeam = withRouter(({
   router,
   login,
 }) : Element => (
-  <div className={styles.createTeam}>
-    <Heading kind='huge'>
-      <FormattedMessage
-        id='createTeam'
-        defaultMessage='Create Team'
+  <div className={styles.center}>
+    <Well>
+      <Heading>
+        <FormattedMessage
+          id='createTeam'
+          defaultMessage='Create Team'
+        />
+      </Heading>
+      <CreateTeamForm
+        players={players}
+        teams={teams}
+        onSubmit={(props) => {
+          return addTeam({
+            ...props,
+          }, login).then(() => {
+            router.push('/ladder');
+          }).catch((errors) => {
+            // TODO: Error object handling
+            return Promise.reject(errors);
+          });
+        }}
       />
-    </Heading>
-    <CreateTeamForm
-      players={players}
-      teams={teams}
-      onSubmit={(props) => {
-        return addTeam({
-          ...props,
-        }, login).then(() => {
-          router.push('/ladder');
-        }).catch((errors) => {
-          // TODO: Error object handling
-          return Promise.reject(errors);
-        });
-      }}
-    />
+    </Well>
   </div>
 ));
 
