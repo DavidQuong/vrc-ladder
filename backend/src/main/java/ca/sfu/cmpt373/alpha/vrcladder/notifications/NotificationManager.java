dbcd5dde3ad56fa174435a9646d7b385ff13b65f
HassanFahad <alsultan.ca@gmail.com>
2016-07-05 04:12:43 -0700

    Notifications (Stage 1)
    - Email was sent through the method and received successfully.

---
+++ backend/src/main/java/ca/sfu/cmpt373/alpha/vrcladder/notifications/NotificationManager.java
@@ -0,0 +1,28 @@
+package ca.sfu.cmpt373.alpha.vrcladder.notifications;
+
+
+import ca.sfu.cmpt373.alpha.vrcladder.teams.Team;
+import ca.sfu.cmpt373.alpha.vrcladder.users.User;
+
+public class NotificationManager {
+
+    public NotificationManager(){
+
+    }
+
+    public void notifyTeamCreated(String receiver, User user, Team team){
+
+    }
+
+    public void notifyRegisteration(){
+
+    }
+
+    public void notifyGameScore(){
+
+    }
+
+    public void notifyGameScheduled(){
+
+    }
+}
---
+++ backend/src/main/java/ca/sfu/cmpt373/alpha/vrcladder/notifications/methods/SMS.java
@@ -0,0 +1,7 @@
+package ca.sfu.cmpt373.alpha.vrcladder.notifications.methods;
+
+/**
+ * Created by Hassan Fahad on 2016-07-02.
+ */
+public class SMS {
+}
