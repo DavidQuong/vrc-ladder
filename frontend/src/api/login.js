f37113c8cd0b63302e48b4a3b8edd60d921637e1
htusiime <htusiime@sfu.ca>
2016-07-04 22:56:55 -0700

    added 'http://vrcladder.p76biyyfpm.us-west-2.elasticbeanstalk.com/' to api call files to enable connection with aws server

--- frontend/src/api/login.js
+++ frontend/src/api/login.js
@@ -1,4 +1,4 @@
-const root = 'http://localhost:4567/';
+const root = 'http://vrcladder.p76biyyfpm.us-west-2.elasticbeanstalk.com/';
 
 export const logInUser = (user) => {
   return fetch(`${root}login`, {
