e7e160a1d294583a696c8fc24fc8a79cd4c6b515
tclellan <tclellan@sfu.ca>
2016-07-11 13:59:29 -0700

    Make GetSchedule() Return Teams in MatchGroups
    - make CourSerializer call MatchGroupSerializer to serialize a MatchGroup object

--- backend/src/main/java/ca/sfu/cmpt373/alpha/vrcrest/datatransfer/responses/CourtSerializer.java
+++ backend/src/main/java/ca/sfu/cmpt373/alpha/vrcrest/datatransfer/responses/CourtSerializer.java
@@ -24,7 +24,7 @@ public class CourtSerializer implements JsonSerializer<Court> {
         Map<PlayTime, MatchGroup> matches = src.getScheduledMatches();
         for (PlayTime playTime : matches.keySet()) {
             MatchGroup matchGroup = matches.get(playTime);
-            jsonCourt.addProperty(playTime.toString(), matchGroup.getId().getValue());
+            jsonCourt.addProperty(playTime.toString(), context.serialize(matchGroup).toString());
         }
 
         return jsonCourt;
