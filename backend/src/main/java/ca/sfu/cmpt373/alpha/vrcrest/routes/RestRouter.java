ae455e09e4b1ddd1c8a2e96c8bd95b06a910a81e
dquong <dquong@sfu.ca>
2016-07-10 17:27:15 -0700

    Create extractUserFromRequest method
    Create extractUserFromRequest method in RestRouter class, which will be
    used for authorization purposes.

--- backend/src/main/java/ca/sfu/cmpt373/alpha/vrcrest/routes/RestRouter.java
+++ backend/src/main/java/ca/sfu/cmpt373/alpha/vrcrest/routes/RestRouter.java
@@ -8,11 +8,13 @@ import ca.sfu.cmpt373.alpha.vrcladder.users.authorization.UserAction;
 import ca.sfu.cmpt373.alpha.vrcladder.users.personal.UserId;
 import ca.sfu.cmpt373.alpha.vrcrest.security.RouteSignature;
 import com.google.gson.Gson;
+import spark.Request;
 
 import java.util.List;
 
 public abstract class RestRouter {
 
+    public static final String HEADER_AUTHORIZATION = "Authorization";
     public static final String PARAM_ID = ":id";
 
     public static final String JSON_PROPERTY_ERROR = "error";
@@ -31,9 +33,10 @@ public abstract class RestRouter {
         gson = buildGson();
     }
 
-    protected void verifyUserAuthorization(UserId userId, UserAction action) {
-        User user = userManager.getById(userId);
-        securityManager.checkUserPermission(user, action);
+    protected User extractUserFromRequest(Request request) {
+        String authorizationToken = request.headers(HEADER_AUTHORIZATION);
+        UserId userId = securityManager.parseToken(authorizationToken);
+        return userManager.getById(userId);
     }
 
     protected Gson getGson() {
