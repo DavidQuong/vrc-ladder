014e2f5fd8bad9b481174d40114c331e6f92b374
Karan Sharma <ksa95@sfu.ca>
2016-07-14 17:45:33 -0700

    Style profile/matchgroups css

--- frontend/src/config/colors.config.js
+++ frontend/src/config/colors.config.js
@@ -2,14 +2,16 @@ export const white = '#fff';
 export const offWhite = '#FAEBD7';
 export const greyDark = '#444';
 
+/* wood theme */
 export const darkGreen = '#01A108';
 export const darkerGreen = '#008409';
 export const paleGreen = '#D0F8B2';
-export const charcoalGrey = '#424242';
 export const honeydewWhite = '#F2FFE9';
-export const paleYellow = '#E8FF8E';
-export const brightYellow = '#DFE029';
 export const woodBrown = '#eacda3';
+export const paleYellow = '#E8FF8E';
 
+/* customer specified theme */
 export const brightBlue = '#007FFF';
 export const paleBlue = '#76ADFF';
+export const brightYellow = '#DFE029';
+export const charcoalGrey = '#424242';
