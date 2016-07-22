aa11166d4a6a4de6b40d33ff331da8aac034d6b6
Peter <pt.4all@hotmail.com>
2016-07-21 18:03:42 -0700

    update UserRouter.  Password updates with the rest of user now

--- frontend/src/component/profile/profile.js
+++ frontend/src/component/profile/profile.js
@@ -24,6 +24,98 @@ const validate = (values, userInfo) => {
   }
   return errors;
 };
+
+const updateProfileInfo = ({
+ fields: {
+   userId,
+   firstName,
+   lastName,
+   emailAddress,
+   phoneNumber,
+   password,
+   confirmPassword},
+ handleSubmit,
+}) => (
+ <Form horizontal onSubmit={handleSubmit}>
+   <div>
+     <FormGroup>
+       <Col componentClass={ControlLabel} sm={4}>User ID</Col>
+       <Col sm={4}>
+         <FormControl type='userID' placeholder='Your ID #' {...userId} />
+       </Col>
+       <Col sm={3}><FormError {...userId}/></Col>
+     </FormGroup>
+
+     <FormGroup>
+       <Col componentClass={ControlLabel} sm={4}>First Name</Col>
+       <Col sm={4}>
+         <FormControl
+           type='firstName'
+           placeholder='eg. Jane'
+           {...firstName}
+         />
+       </Col>
+       <Col sm={3}><FormError {...firstName}/></Col>
+     </FormGroup>
+
+     <FormGroup>
+       <Col componentClass={ControlLabel} sm={4}>Last Name</Col>
+       <Col sm={4}>
+         <FormControl type='lastName' placeholder='eg. Doe' {...lastName} />
+       </Col>
+       <Col sm={3}><FormError {...lastName}/></Col>
+     </FormGroup>
+
+     <FormGroup>
+       <Col componentClass={ControlLabel} sm={4}>Email</Col>
+       <Col sm={5}>
+         <FormControl
+           type='emailAddress'
+           placeholder='eg. jane.doe@example.com'
+           {...emailAddress}
+         />
+       </Col>
+       <Col sm={3}><FormError {...emailAddress}/></Col>
+     </FormGroup>
+
+     <FormGroup>
+       <Col componentClass={ControlLabel} sm={4}>Phone Number</Col>
+       <Col sm={4}>
+         <FormControl
+           type='phoneNumber'
+           placeholder='eg. 6041234567'
+           {...phoneNumber}
+         />
+       </Col>
+       <Col sm={3}><FormError {...phoneNumber}/></Col>
+     </FormGroup>
+
+     <FormGroup>
+       <Col componentClass={ControlLabel} sm={4}>Password</Col>
+       <Col sm={5}>
+         <FormControl type='password' placeholder='Password' {...password} />
+       </Col>
+       <Col sm={3}><FormError {...password}/></Col>
+     </FormGroup>
+
+     <FormGroup>
+       <Col componentClass={ControlLabel} sm={4}>Confirm Password</Col>
+       <Col sm={5}>
+         <FormControl
+           type='password'
+           placeholder='Password confirmation'
+           {...confirmPassword}
+         />
+       </Col>
+       <Col sm={3}><FormError {...confirmPassword}/></Col>
+     </FormGroup>
+
+     <div className={classNames(styles.center)}>
+       <Button bsStyle='primary' bsSize='large' type='submit'>Sign Up</Button>
+     </div>
+   </div>
+ </Form>
+);
 const validateUpdateStatus = (values) => {
   const errors = {};
   if (!values.playTime) {
