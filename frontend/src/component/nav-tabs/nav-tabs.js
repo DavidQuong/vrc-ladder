bd48db419da1708eb7498f8cffe2c681fe96fbc6
Trevor Clelland <trevy17@gmail.com>
2016-07-24 00:48:13 -0700

    Create Dynamic Tabs
    - Only Login and Ladder Tabs appear if not logged in
    - Once logged in, the rest of the tabs appear

--- frontend/src/component/nav-tabs/nav-tabs.js
+++ frontend/src/component/nav-tabs/nav-tabs.js
@@ -1,67 +1,75 @@
-// import React, {createElement} from 'react';
-import {createElement} from 'react';
+import React, {createElement} from 'react';
+// import {createElement} from 'react';
 import {Nav, NavItem} from 'react-bootstrap';
 import {LinkContainer} from 'react-router-bootstrap';
 import {connect} from 'react-redux';
 import styles from './nav-tabs.css';
 
-const NavTabsDummy = (userInfo) => (
-  userInfo.firstName ?
-  (<Nav pullRight className={styles.navBar}>
-    <LinkContainer to='/'>
-      <NavItem>Log in</NavItem>
-    </LinkContainer>
-    <LinkContainer to='/profile'>
-      <NavItem>Profile</NavItem>
-    </LinkContainer>
-    <LinkContainer to='/ladder'>
-      <NavItem>Ladder</NavItem>
-    </LinkContainer>
-    <LinkContainer to='/match-groups'>
-      <NavItem>Match Groups</NavItem>
-    </LinkContainer>
-    <LinkContainer to='/signup'>
-      <NavItem>Sign up</NavItem>
-    </LinkContainer>
-  </Nav>) :
-  <Nav pullRight className={styles.navBar}>
-    <LinkContainer to='/'>
-      <NavItem>Log in</NavItem>
-    </LinkContainer>
-    <LinkContainer to='/ladder'>
-      <NavItem>Ladder</NavItem>
-    </LinkContainer>
-  </Nav>
-);
+// const NavTabsDummy = (userInfo) => (
+//   // true ?
+//   (<Nav pullRight className={styles.navBar}>
+//     <LinkContainer to='/'>
+//       <NavItem>Log out</NavItem>
+//     </LinkContainer>
+//     <LinkContainer to='/profile'>
+//       <NavItem>Profile</NavItem>
+//     </LinkContainer>
+//     <LinkContainer to='/ladder'>
+//       <NavItem>Ladder</NavItem>
+//     </LinkContainer>
+//     <LinkContainer to='/match-groups'>
+//       <NavItem>Match Groups</NavItem>
+//     </LinkContainer>
+//     <LinkContainer to='/signup'>
+//       <NavItem>Sign up</NavItem>
+//     </LinkContainer>
+//   </Nav>)
+//   // <Nav pullRight className={styles.navBar}>
+//   //   <LinkContainer to='/'>
+//   //     <NavItem>Log in</NavItem>
+//   //   </LinkContainer>
+//   //   <LinkContainer to='/ladder'>
+//   //     <NavItem>Ladder</NavItem>
+//   //   </LinkContainer>
+//   // </Nav>
+// );
 
 // module.exports = NavTabsDummy;
 
-// export const NavTabsDummy = React.createClass({
-//   render() {
-//     return (
-//       <div>
-//         <LinkContainer to='/'>
-//           <NavItem>Log in</NavItem>
-//         </LinkContainer>
-//         <LinkContainer to='/profile'>
-//           <NavItem>Profile</NavItem>
-//         </LinkContainer>
-//         <LinkContainer to='/ladder'>
-//           <NavItem>Ladder</NavItem>
-//         </LinkContainer>
-//         <LinkContainer to='/match-groups'>
-//           <NavItem>Match Groups</NavItem>
-//         </LinkContainer>
-//         <LinkContainer to='/signup'>
-//           <NavItem>Sign up</NavItem>
-//         </LinkContainer>
-//       </div>);
-//   },
-// });
+export const NavTabsDummy = React.createClass({
+  render() {
+    return (this.props.loggedIn.authorizationToken) ?
+      (<Nav pullRight>
+        <LinkContainer to='/'>
+          <NavItem>Log out</NavItem>
+        </LinkContainer>
+        <LinkContainer to='/profile'>
+          <NavItem>Profile</NavItem>
+        </LinkContainer>
+        <LinkContainer to='/ladder'>
+          <NavItem>Ladder</NavItem>
+        </LinkContainer>
+        <LinkContainer to='/match-groups'>
+          <NavItem>Match Groups</NavItem>
+        </LinkContainer>
+        <LinkContainer to='/signup'>
+          <NavItem>Sign up</NavItem>
+        </LinkContainer>
+      </Nav>) :
+      (<Nav pullRight className={styles.navBar}>
+       <LinkContainer to='/'>
+         <NavItem>Log in</NavItem>
+       </LinkContainer>
+       <LinkContainer to='/ladder'>
+         <NavItem>Ladder</NavItem>
+       </LinkContainer>
+      </Nav>);
+  },
+});
 
 const mapStateToProps = (state) => (
   {
-    userInfo: state.app.userInfo,
+    loggedIn: state.app.loggedIn,
   }
 );
 
