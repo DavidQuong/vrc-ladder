e095ac5908f0e2353de5f2710e37790a92c4efe6
Eddie Marchioni <emarchio@sfu.ca>
2016-07-27 17:56:03 -0700

    Finish new MatchScheduler algorithm

--- backend/src/main/java/ca/sfu/cmpt373/alpha/vrcladder/matchmaking/logic/MatchScheduler.java
+++ backend/src/main/java/ca/sfu/cmpt373/alpha/vrcladder/matchmaking/logic/MatchScheduler.java
@@ -4,22 +4,20 @@ import ca.sfu.cmpt373.alpha.vrcladder.exceptions.MatchMakingException;
 import ca.sfu.cmpt373.alpha.vrcladder.matchmaking.Court;
 import ca.sfu.cmpt373.alpha.vrcladder.matchmaking.MatchGroup;
 import ca.sfu.cmpt373.alpha.vrcladder.teams.attendance.PlayTime;
-import com.fasterxml.jackson.databind.deser.DataFormatReaders;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MatchScheduler {
-    private static final String ERROR_MESSAGE_COURTS_FULL = "There were not enough courts available to schedule all matches";
-    private static final String ERROR_MESSAGE_NOT_ATTENDING = "A team in a group has indicated they are not attending.";
+    private static final String ERROR_MESSAGE_NOT_ATTENDING = "A group has indicated they are not attending.";
 
     /**
-     * @throws MatchMakingException if Courts are full, and matches cannot be scheduled
+     * @throws MatchMakingException if a group has indicated they are not attending
      */
     public static List<Court> scheduleMatches(List<MatchGroup> matchGroups) {
         // Schedule matches into the courts.
         List<Court> courts = new ArrayList<>();
-        for (int i = 0; i < matchGroups.size(); i++) {
+        for(int i = 0;i < matchGroups.size();i++) {
             courts.add(new Court());
         }
 
@@ -42,42 +40,24 @@ public class MatchScheduler {
                     throw new MatchMakingException(ERROR_MESSAGE_NOT_ATTENDING);
                 }
             }
+
+            while(slotAGroups.size() - slotBGroups.size() > 1) {
+                slotBGroups.add(slotAGroups.remove(slotAGroups.size() - 1));
+            }
+
+            while(slotBGroups.size() - slotAGroups.size() > 1) {
+                slotAGroups.add(slotBGroups.remove(slotBGroups.size() - 1));
+            }
+
+            for(int i = 0;i < slotAGroups.size();i++) {
+                courts.get(i).scheduleMatch(slotAGroups.get(i), PlayTime.TIME_SLOT_A);
+            }
+
+            for(int i = 0;i < slotBGroups.size();i++) {
+                courts.get(i).scheduleMatch(slotBGroups.get(i), PlayTime.TIME_SLOT_B);
+            }
         }
 
-        /*for (MatchGroup matchGroup : matchGroups) {
-            PlayTime preferredPlayTime = matchGroup.getPreferredGroupPlayTime();
-            preferredPlayTime.checkPlayablePlayTime();
-
-            //try to schedule a match group at their preferred time
-            boolean isPreferredPlayTimeScheduled = false;
-            for (Court court : courts) {
-                if (court.isPlayTimeFree(preferredPlayTime)) {
-                    court.scheduleMatch(matchGroup, preferredPlayTime);
-                    isPreferredPlayTimeScheduled = true;
-                    break;
-                }
-            }
-
-            //if no courts have the available preferred play time for a group, choose another play time to schedule them in
-            if (!isPreferredPlayTimeScheduled) {
-                boolean isAnyPlayTimeScheduled = false;
-                for (Court court : courts) {
-                    for (PlayTime playTime : PlayTime.values()) {
-                        if (playTime.isPlayable() && court.isPlayTimeFree(playTime)) {
-                            court.scheduleMatch(matchGroup, playTime);
-                            isAnyPlayTimeScheduled = true;
-                            break;
-                        }
-                    }
-                    if (isAnyPlayTimeScheduled) {
-                        break;
-                    }
-                }
-                if (!isAnyPlayTimeScheduled) {
-                    throw new MatchMakingException(ERROR_MESSAGE_COURTS_FULL);
-                }
-            }
-        }*/
         return courts;
     }
 }
--- backend/src/test/java/ca/sfu/cmpt373/alpha/vrcladder/matchmaking/logic/MatchSchedulerTest.java
+++ backend/src/test/java/ca/sfu/cmpt373/alpha/vrcladder/matchmaking/logic/MatchSchedulerTest.java
@@ -10,34 +10,19 @@ import java.util.List;
 
 
 public class MatchSchedulerTest {
-    private static final int NUM_COURTS = 6;
-
-    //TODO: add more tests!
 
     @Test
     public void testMaxTeamCount() {
         int testGroupCount = 12;
-        List<Court> courts = MatchScheduler.scheduleMatches(
-                NUM_COURTS,
-                MockDatabase.getMockMatchGroups(testGroupCount));
+        List<Court> courts = MatchScheduler.scheduleMatches(MockDatabase.getMockMatchGroups(testGroupCount));
 
         //check that all courts are filled
         for (Court court : courts) {
-            for (PlayTime playTime : PlayTime.values()) {
-                if (playTime.isPlayable()) {
+            for(PlayTime playTime : PlayTime.values()) {
+                if(playTime.isPlayable()) {
                     Assert.assertTrue(!court.isPlayTimeFree(playTime));
                 }
             }
         }
     }
-
-    /*
-    @Test (expected = MatchMakingException.class)
-    public void testCourtsFull() {
-        int testGroupCount = 13;
-        MatchScheduler.scheduleMatches(
-                NUM_COURTS,
-                MockDatabase.getMockMatchGroups(testGroupCount));
-    }
-    */
 }
