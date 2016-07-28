343ceda2da8f346b6843e58e92ccddddc7d47439
Trevor Clelland <trevy17@gmail.com>
2016-07-27 18:26:21 -0700

    Fix Admin MatchGroup View
    - 4 team Matchgroups now show up properly
    - Teams are now selectable again

--- frontend/src/component/matchgroups/admin/matchgroups-admin.js
+++ frontend/src/component/matchgroups/admin/matchgroups-admin.js
@@ -6,18 +6,29 @@ import {
   reportMatchResults,
   regenerateMatchGroups} from '../../../action/matchgroups';
 import {withRouter} from 'react-router';
-import {reduxForm} from 'redux-form';
-import {FormattedMessage} from 'react-intl';
-import {SubmitBtn} from '../../button';
-import {Form, Panel, Well} from 'react-bootstrap';
-
-import Heading from '../../heading/heading';
 import map from 'lodash/fp/map';
-import isEmpty from 'lodash/fp/isEmpty';
 import reduce from 'lodash/fp/reduce';
 import find from 'lodash/fp/find';
 import classNames from 'classnames';
 import styles from './matchgroups-admin.css';
+import {ResultForm} from '../../match-results/result-form';
+import {Well, Panel} from 'react-bootstrap';
+import isEmpty from 'lodash/fp/isEmpty';
+
+const validate = (values) => {
+  const errors = {};
+  if (values.teamId1 === values.teamId2) {
+    errors.teamId1 = 'Cannot be same team';
+    errors.teamId2 = 'Cannot be same team';
+  } else if (values.teamId1 === values.teamId3) {
+    errors.teamId1 = 'Cannot be same team';
+    errors.teamId3 = 'Cannot be same team';
+  } else if (values.teamId2 === values.teamId3) {
+    errors.teamId2 = 'Cannot be same team';
+    errors.teamId3 = 'Cannot be same team';
+  }
+  return errors;
+};
 
 const getTeams = (state) => {
   const matchGroups = state.app.matchGroups;
@@ -44,21 +55,6 @@ const getTeams = (state) => {
   );
 };
 
-const validate = (values) => {
-  const errors = {};
-  if (values.teamId1 === values.teamId2) {
-    errors.teamId1 = 'Cannot be same team';
-    errors.teamId2 = 'Cannot be same team';
-  } else if (values.teamId1 === values.teamId3) {
-    errors.teamId1 = 'Cannot be same team';
-    errors.teamId3 = 'Cannot be same team';
-  } else if (values.teamId2 === values.teamId3) {
-    errors.teamId2 = 'Cannot be same team';
-    errors.teamId3 = 'Cannot be same team';
-  }
-  return errors;
-};
-
 const matchGroupTeams = ({matchGroup, teams}) => {
   const teamIds = matchGroup.teamId4 ? [
     matchGroup.teamId1,
@@ -74,300 +70,40 @@ const matchGroupTeams = ({matchGroup, teams}) => {
   );
 };
 
-const ResultFormThree = reduxForm({
-  form: 'resultFour',
-  fields: ['teamId1', 'teamId2', 'teamId3'],
-  validate,
-})(({
-  fields: {teamId1, teamId2, teamId3},
-  matchTeams,
-  handleSubmit,
-}) => (
-  <Form horizontal onSubmit={handleSubmit}>
-  <div className={classNames(styles.formGroup)}>
-    <label
-      className={classNames(styles.colXsTitle)}
-    >
-      <FormattedMessage
-        id='teamId1'
-        defaultMessage='Rank 1:'
-      />
-    </label>
-    <select
-      className={classNames(styles.goodForm, {
-        [styles.errorForm]: teamId1.error &&
-                            teamId1.touched})}
-      {...teamId1}
-    >
-      <option value=''>Select a team...</option>
-      {map((teams) => (
-        <option value={teams.teamId}key={teams.teamId}>
-          {teams.firstPlayer.name} & {teams.secondPlayer.name}
-        </option>
-      ), matchTeams)}
-    </select>
-    {teamId1.touched && teamId1.error &&
-      <div className={classNames(styles.errorMsg)}>
-      <Heading kind='error'>
-            {teamId1.error}
-          </Heading>
-      </div>}
-  </div>
-  <div className={classNames(styles.formGroup)}>
-    <label
-      className={classNames(styles.colXsTitle)}
-    >
-      <FormattedMessage
-        id='teamId2'
-        defaultMessage='Rank 2:'
-      />
-    </label>
-    <select
-      className={classNames(styles.goodForm, {
-        [styles.errorForm]: teamId2.error &&
-                            teamId2.touched})}
-      {...teamId2}
-    >
-      <option value=''>Select a team...</option>
-      {map((teams) => (
-        <option value={teams.teamId}key={teams.teamId}>
-          {teams.firstPlayer.name} & {teams.secondPlayer.name}
-        </option>
-      ), matchTeams)}
-    </select>
-    {teamId2.touched && teamId2.error &&
-      <div className={classNames(styles.errorMsg)}>
-      <Heading kind='error'>
-            {teamId2.error}
-          </Heading>
-      </div>}
-  </div>
-  <div className={classNames(styles.formGroup)}>
-    <label
-      className={classNames(styles.colXsTitle)}
-    >
-      <FormattedMessage
-        id='teamId3'
-        defaultMessage='Rank 3:'
-      />
-    </label>
-    <select
-      className={classNames(styles.goodForm, {
-        [styles.errorForm]: teamId3.error &&
-                            teamId3.touched})}
-      {...teamId3}
-    >
-      <option value=''>Select a team...</option>
-      {map((teams) => (
-        <option value={teams.teamId}key={teams.teamId}>
-          {teams.firstPlayer.name} & {teams.secondPlayer.name}
-        </option>
-      ), matchTeams)}
-    </select>
-    {teamId3.touched && teamId3.error &&
-      <div className={classNames(styles.errorMsg)}>
-      <Heading kind='error'>
-            {teamId3.error}
-          </Heading>
-      </div>}
-  </div>
-  <div className={classNames(styles.center)}>
-    <SubmitBtn type='submit'>Submit Results</SubmitBtn>
-  </div>
-  </Form>
-));
-
-const ResultFormFour = reduxForm({
-  form: 'resultFour',
-  fields: ['teamId1', 'teamId2', 'teamId3', 'teamId4'],
-})(({
-  fields: {teamId1, teamId2, teamId3, teamId4},
-  matchTeams,
-  handleSubmit,
-}) => (
-  <Form horizontal onSubmit={handleSubmit}>
-  <div className={classNames(styles.formGroup)}>
-    <label
-      className={classNames(styles.colXsTitle)}
-    >
-      <FormattedMessage
-        id='teamId1'
-        defaultMessage='Rank 1:'
-      />
-    </label>
-    <select
-      className={classNames(styles.goodForm, {
-        [styles.errorForm]: teamId1.error &&
-                            teamId1.touched})}
-      {...teamId1}
-    >
-      <option value=''>Select a team...</option>
-      {map((teams) => (
-        <option value={teams.teamId}key={teams.teamId}>
-          {teams.firstPlayer.name} & {teams.secondPlayer.name}
-        </option>
-      ), matchTeams)}
-    </select>
-    {teamId1.touched && teamId1.error &&
-      <div className={classNames(styles.errorMsg)}>
-      <Heading kind='error'>
-            {teamId1.error}
-          </Heading>
-      </div>}
-  </div>
-  <div className={classNames(styles.formGroup)}>
-    <label
-      className={classNames(styles.colXsTitle)}
-    >
-      <FormattedMessage
-        id='teamId2'
-        defaultMessage='Rank 2:'
-      />
-    </label>
-    <select
-      className={classNames(styles.goodForm, {
-        [styles.errorForm]: teamId2.error &&
-                            teamId2.touched})}
-      {...teamId2}
-    >
-      <option value=''>Select a team...</option>
-      {map((teams) => (
-        <option value={teams.teamId}key={teams.teamId}>
-          {teams.firstPlayer.name} & {teams.secondPlayer.name}
-        </option>
-      ), matchTeams)}
-    </select>
-    {teamId2.touched && teamId2.error &&
-      <div className={classNames(styles.errorMsg)}>
-      <Heading kind='error'>
-            {teamId2.error}
-          </Heading>
-      </div>}
-  </div>
-  <div className={classNames(styles.formGroup)}>
-    <label
-      className={classNames(styles.colXsTitle)}
-    >
-      <FormattedMessage
-        id='teamId3'
-        defaultMessage='Rank 3:'
-      />
-    </label>
-    <select
-      className={classNames(styles.goodForm, {
-        [styles.errorForm]: teamId3.error &&
-                            teamId3.touched})}
-      {...teamId3}
-    >
-      <option value=''>Select a team...</option>
-      {map((teams) => (
-        <option value={teams.teamId}key={teams.teamId}>
-          {teams.firstPlayer.name} & {teams.secondPlayer.name}
-        </option>
-      ), matchTeams)}
-    </select>
-    {teamId3.touched && teamId3.error &&
-      <div className={classNames(styles.errorMsg)}>
-      <Heading kind='error'>
-            {teamId3.error}
-          </Heading>
-      </div>}
-  </div>
-  <div className={classNames(styles.formGroup)}>
-    <label
-      className={classNames(styles.colXsTitle)}
-    >
-      <FormattedMessage
-        id='teamId4'
-        defaultMessage='Rank 4:'
-      />
-    </label>
-    <select
-      className={classNames(styles.goodForm, {
-        [styles.errorForm]: teamId4.error &&
-                            teamId4.touched})}
-      {...teamId4}
-    >
-      <option value=''>Select a team...</option>
-      {map((teams) => (
-        <option value={teams.teamId}key={teams.teamId}>
-          {teams.firstPlayer.name} & {teams.secondPlayer.name}
-        </option>
-      ), matchTeams)}
-    </select>
-    {teamId4.touched && teamId4.error &&
-      <div className={classNames(styles.errorMsg)}>
-      <Heading kind='error'>
-            {teamId4.error}
-          </Heading>
-      </div>}
-  </div>
-  <div className={classNames(styles.center)}>
-    <SubmitBtn type='submit'>Submit Results</SubmitBtn>
-  </div>
-  </Form>
-));
-
 const MatchGroupForms = ({matchGroup, teams}) => {
   const matchTeams = matchGroupTeams({matchGroup, teams});
   return (
     <Well>
-      <Panel header={`Court ID: ${matchGroup.matchGroupId}`} bsStyle='primary'>
+      <Panel header={`Group ID: ${matchGroup.matchGroupId}`} bsStyle='primary'>
         <div className={classNames(styles.teamNames)}>
           {matchTeams.map((team) => (
             <div key={team.teamId}>
-              {team.firstPlayer.name} & {team.secondPlayer.name} & {team.teamId}
+              {team.firstPlayer.name} & {team.secondPlayer.name}
             </div>
           ))}
         </div>
       </Panel>
       <Panel header='Result Submission' bsStyle='primary'>
-      {matchGroup.teamId4 ?
-          <ResultFormFour
-            matchTeams={matchTeams}
-            onSubmit={(props) => {
-              const errors = validate(props);
-              if (!isEmpty(errors)) {
-                return Promise.reject(errors);
-              }
-              console.log('submitted: ', props);
-              console.log('matchgroup: ', matchGroup.matchGroupId);
-              console.log(reportMatchResults);
-              return reportMatchResults({
-                ...props,
-                ...matchGroup.matchGroupId,
-              }).then((response) => {
-                console.log("Submitted: ", response);
-                return Promise.resolve();
-              }).catch((errors) => {
-                console.log('errors: ', errors);
-                return Promise.reject(errors);
-              });
-            }}
-          />  :
-          <ResultFormThree
-            matchTeams={matchTeams}
-            onSubmit={(props) => {
-              const errors = validate(props);
-              if (!isEmpty(errors)) {
-                return Promise.reject(errors);
-              }
-              console.log('submitted: ', props);
-              console.log('matchgroup: ', matchGroup.matchGroupId);
-              console.log(reportMatchResults);
-              return reportMatchResults({
-                ...props,
-                ...matchGroup.matchGroupId,
-              }).then((response) => {
-                console.log("Submitted: ", response);
-                return Promise.resolve();
-              }).catch((errors) => {
-                console.log('errors: ', errors);
-                return Promise.reject(errors);
-              });
-            }}
-          />}
-        </Panel>
+        {ResultForm(matchGroup, matchTeams, (props) => {
+          const errors = validate(props);
+          if (!isEmpty(errors)) {
+            return Promise.reject(errors);
+          }
+          console.log('submitted: ', props);
+          console.log('matchgroup: ', matchGroup.matchGroupId);
+          console.log(reportMatchResults);
+          return reportMatchResults({
+            ...props,
+            ...matchGroup.matchGroupId,
+          }).then((response) => {
+            console.log("Submitted: ", response);
+            return Promise.resolve();
+          }).catch((errors) => {
+            console.log('errors: ', errors);
+            return Promise.reject(errors);
+          });
+        })}
+      </Panel>
     </Well>
   );
 };
