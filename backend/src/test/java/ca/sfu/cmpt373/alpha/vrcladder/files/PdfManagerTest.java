a3a5510fe777f7fe29d88d0a9f11e9f8e21f9c68
HassanFahad <alsultan.ca@gmail.com>
2016-08-02 14:26:45 -0700

    Email PDF
    - Fix test case.
    - Fixes #109

--- backend/src/test/java/ca/sfu/cmpt373/alpha/vrcladder/files/PdfManagerTest.java
+++ backend/src/test/java/ca/sfu/cmpt373/alpha/vrcladder/files/PdfManagerTest.java
@@ -5,6 +5,12 @@ import ca.sfu.cmpt373.alpha.vrcladder.file.PdfManager;
 import ca.sfu.cmpt373.alpha.vrcladder.ladder.Ladder;
 import ca.sfu.cmpt373.alpha.vrcladder.teams.Team;
 import ca.sfu.cmpt373.alpha.vrcladder.teams.attendance.PlayTime;
+import ca.sfu.cmpt373.alpha.vrcladder.users.User;
+import ca.sfu.cmpt373.alpha.vrcladder.users.authentication.Password;
+import ca.sfu.cmpt373.alpha.vrcladder.users.authorization.UserRole;
+import ca.sfu.cmpt373.alpha.vrcladder.users.personal.EmailAddress;
+import ca.sfu.cmpt373.alpha.vrcladder.users.personal.PhoneNumber;
+import ca.sfu.cmpt373.alpha.vrcladder.users.personal.UserId;
 import ca.sfu.cmpt373.alpha.vrcladder.util.MockTeamGenerator;
 
 import java.util.ArrayList;
@@ -12,6 +18,13 @@ import java.util.List;
 
 public class PdfManagerTest {
     public static void main(String[] args){
+        UserId userId = new UserId("100");
+        EmailAddress email = new EmailAddress("alsultan.ca@gmail.com");
+        PhoneNumber number = new PhoneNumber("778-227-1620");
+        Password password = new Password("M49D4s5");
+        User user = new User(userId, UserRole.VOLUNTEER, "Hassan", "Fahad", "Alsultan", email, number, password);
+
+
         List<Team> teams = new ArrayList<>();
         for(int counter = 0; counter < 33; counter++){
             teams.add(MockTeamGenerator.generateTeam());
@@ -21,6 +34,6 @@ public class PdfManagerTest {
         firstTeam.getAttendanceCard().setPreferredPlayTime(PlayTime.TIME_SLOT_A);
 
         PdfManager pdf = new PdfManager(ladder);
-        pdf.exportLadder();
+        pdf.exportLadder(user);
     }
 }
